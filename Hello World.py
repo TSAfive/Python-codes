1166b3aa365d96f5184a719a033105c3a1c528d8
aravindts <tsaitprofessional@gmail.com>
2023-05-31 10:39:24 +0530

    hellow world

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+print("Hello World\n Refreshing Python")
+a = 5
+b = 10
+c = 15
+sum = a + b + c
+print(sum)
+if (a == 4):
+    print("True")
+if (b == 7):
+    print("True")
+else :
+    print("False")
