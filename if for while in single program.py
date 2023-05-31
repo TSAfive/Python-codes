dffd0b7d440b727e05859467794894adbaa78f13
TS <43196901+TSAfive@users.noreply.github.com>
2023-05-31 10:43:51 +0530

    Create if for while in single program.py

---
+++ if for while in single program.py
@@ -0,0 +1,29 @@
+a = 5
+print(a)
+if a == 5:
+    print("Five")
+b = 10
+if b != 10:
+    print("b is not equal to ten")
+else:
+    print("b is equal to ten")
+Names = ["India", "Russia", "Japan"]
+for Name in Names:
+    print(Names)
+c = 5
+while c < 10:
+    print(c)
+    c += 1
+d = 27
+while d < 36:
+    if d == 30:
+      break
+    print(d)
+    d  += 1
+g = 50
+while g < 60:
+   g += 1
+   if g != 55:
+    continue
+   print(g)
+   
