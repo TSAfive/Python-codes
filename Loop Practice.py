bb078ec5299475e3ae23b47ba7a7f05a460bef78
aravindts <tsaitprofessional@gmail.com>
2023-05-31 10:23:57 +0530

    Loop practice

---
+++ Loop Practice.py
@@ -0,0 +1,73 @@
+#Pratice if loop
+'''
+a = 50
+b = 40
+if (a > b):
+    print("a is greater than b")
+'''
+
+#Practice nested if
+'''
+a = 30
+b = 20
+if ( a == b):
+    print("a is equal to b")
+if (a < b):
+    print("a is lesser than b")
+else:
+    print("a is greater than b")
+    '''
+    
+#Elif Practice
+''''
+a = 60
+b = 60
+if (a > b):
+    print("a is greater than b")
+elif(a == b):
+    print("a is equal to b")
+    '''
+    
+#While loop Practice - executes until the condition is true
+
+''''
+a = 5
+while a < 9:
+    print(a)
+    a += 2
+    '''
+    
+#While loop practice - with break statement
+'''
+a = 10
+while a < 20:
+    print(a)
+    if (a == 18):
+        break
+    a+= 2
+'''
+    
+#While loop practice - with continue statement, number 30 is missing in the output because in condition a == 30
+'''
+a = 20
+while (a < 30):
+    a+= 3
+    if (a == 30):
+        continue
+    print(a)
+'''
+
+#for loop practice - it can be used with list, tuple and dictionary
+'''
+b = ["Gauva", "Mango", "Pineapple"]
+for y in b:
+    print(y)
+''' 
+#for loop practice with range numbers - this will increment the value by 3, the output will start with 3 and increments by 2 till 60
+
+'''
+for c in range(3,60,2):
+    print(c)
+'''    
+    
+
